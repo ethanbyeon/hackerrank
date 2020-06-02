161540413c6ac4864f4c384f6b5d554c1b1a351c
ethanbyeon <51931482+ethanbyeon@users.noreply.github.com>
2020-06-01 22:53:11 -0700

    learned about EOF and static initializer blocks

---
+++ eof.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class eof {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        int count = 1;
+        while(in.hasNext()) {
+            String line = in.nextLine();
+            System.out.println(count + " " + line);
+            count++;
+        }
+        
+        in.close();
+    }
+}
---
+++ staticInitializerBlock.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class staticInitializerBlock {
+    
+    static boolean flag = true;
+    static int B, H;
+
+    static {
+        
+        Scanner in = new Scanner(System.in);
+        B = in.nextInt();
+        H = in.nextInt();
+        in.close();
+
+        if(B > 0 && H > 0) {
+            flag = false;
+        }else if((B <= 0 && H >= 0) || (B >= 0 && H > 0)) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }else {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) { }
+}
